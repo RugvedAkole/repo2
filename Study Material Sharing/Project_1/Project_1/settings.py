ec7a863b542f96a6293b81dbd76d4ec86d3c2be0
RugvedAkole <akolerugved3242@gmail.com>
2026-04-23 00:21:31 +0530

    Update settings.py

--- Study Material Sharing/Project_1/Project_1/settings.py
+++ Study Material Sharing/Project_1/Project_1/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-lx+^e5v(#5vw^-l^a149k1u*#c-5l4rsw0)&xcm%twb@k0zkhz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = []
 
 
 # Application definition
